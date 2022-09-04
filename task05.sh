02d2592d71aed13e4ffcea24fb7228c2a981d545
name_snrl <Demogorgon-74@ya.ru>
2022-09-05 02:28:19 +0500

    Add the answer to the 5th

--- task05.sh
+++ task05.sh
@@ -1,3 +1,39 @@
 #!/usr/bin/env bash
 
+interval=3
+url=localhost:80
 
+sender=zxc123zxc.pupkin@mail.ru
+password=$(< mail_pass)
+recipient=demogorgon-74@ya.ru
+
+status=alive
+
+while true; do
+
+    if [[ $status == alive ]]; then
+        if curl -Is "$url" > /dev/null; then
+            status=alive
+            echo ALIVE
+            sleep "$interval"
+        else
+            status=dead
+            echo DIED
+            curl smtps://smtp.mail.ru:465 \
+                --ssl-reqd --silent \
+                --user "$sender:$password" \
+                --mail-from "$sender" \
+                --mail-rcpt "$recipient" \
+                --upload-file <(echo Server died!)
+        fi
+    fi
+
+    while [[ $status == dead ]]; do
+        sleep 3
+        if curl -Is "$url" > /dev/null; then
+            status=alive
+        else
+            echo STILL DEAD
+        fi
+    done
+done
